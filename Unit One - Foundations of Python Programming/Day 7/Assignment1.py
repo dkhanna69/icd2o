82ba90334e9b499dfba5d8a22e8ba698b5097439
Dev Khanna <dkhanna@bayviewglen.ca>
2023-10-04 13:56:22 -0400

    Version 1

---
+++ Unit One - Foundations of Python Programming/Day 7/Assignment1.py
@@ -0,0 +1,23 @@
+name = input("Please enter your name: ")
+
+p1 = input("How many points did you get for game 1? ")
+p1o = input("How many points did your oppenent get in game 1? ")
+name1o = input("What was your oppoenents name for game 1? ")
+
+p2 = input("How many points did you get in game 2? ")
+p2o = input("How many points did your opponent get in game 2? ")
+name2o = input("What was your opponents name for game 2: ")
+
+p3 = input("How many points did you get for game 3? ")
+p3o = input("How many points did your opponent get in game 3?")
+name3o = input("What was your opponents name for game 3? ")
+
+p4 = input("How many points did you get in game 4?")
+p4o = input("How many points did your opponent get in game 4?")
+name4o = input("What was your opponents name for game 4? ")
+
+p5 = input("How many points did you get in game 5?")
+p5o = input("How many points did your opponent get in game 5? ")
+name5o = input("What was your opponents name for game 5? ")
+
+
